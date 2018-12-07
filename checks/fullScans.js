ef8f98c6f2bc5d2a3e0101f71bb83a2c35371ba1
Troy Whiteley <troy@somany.us>
2018-12-07 13:24:47 -0800

    Tweak full scan check to only look in the last 30 days

--- checks/fullScans.js
+++ checks/fullScans.js
@@ -1,5 +1,8 @@
 module.exports = async function(log, database, dbName, options, db) {
-  const results = await database.collection('system.profile').find({ nreturned: { $gt: 1 } }).toArray();
+  const results = await database.collection('system.profile').find({
+    nreturned: { $gt: 1 },
+    ts: { $gte: new Date((new Date().getTime() - (30 * 24 * 60 * 60 * 1000))) }
+  }).toArray();
 
   if (!results.length) {
     return false;
