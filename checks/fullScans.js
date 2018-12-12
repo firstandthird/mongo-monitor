c8fb6b307242dd23a013cfb24956562ce964749c
Troy Whiteley <troy@somany.us>
2018-12-11 19:47:15 -0800

    Use last run time for full scan check

--- checks/fullScans.js
+++ checks/fullScans.js
@@ -1,7 +1,7 @@
 module.exports = async function(log, database, dbName, options, db) {
   const results = await database.collection('system.profile').find({
     nreturned: { $gt: 1 },
-    ts: { $gte: new Date((new Date().getTime() - (30 * 24 * 60 * 60 * 1000))) }
+    ts: { $gt: new Date(Date.now() - 1000 * options.interval) }
   }).toArray();
 
   if (!results.length) {
